4a47b865f4f69ade38618510665f4b794c30b6cb
Guido <cavallo.guido.nicolas@gmail.com>
2017-09-04 20:02:02 -0300

    Indice de masa corporal

---
+++ IMC/Imc.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Imc {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+
+        System.out.println("Ingrese la altura:");
+        double altura = teclado.nextFloat();
+
+        System.out.println("Ingrese su peso");
+        double peso = teclado.nextFloat();
+
+        double imc = peso / Math.pow(altura, 2);
+
+        System.out.printf("su indice de masa corporal es: %.2f", imc);
+
+    }
+}
