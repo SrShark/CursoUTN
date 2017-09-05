42b3f575de98a84f1125a8664d4559c4549221f4
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2017-09-04 23:41:47 -0300

    ciclo que imprime la tabla del numero ingresado.

--- ForTabalaMultiplicador/ForTabalaMultiplicador.java
+++ ForTabalaMultiplicador/ForTabalaMultiplicador.java
@@ -4,35 +4,46 @@ public class ForTabalaMultiplicador {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("Ingrese un valor del 1 al 9 para conocer su tabla de multiplicacion: \n");
-
+        System.out.println("Ingrese un valor del 1 al 9 para conocer su tabla de multiplicacion:");
         int numeroIngresado = teclado.nextInt();
 
-        // int[] anArray;
-        // anArray = new int[10];
-        // int[] anArray = {
-        //     100, 200, 300,
-        //     400, 500, 600,
-        //     700, 800, 900, 1000
-        // };
+        int[] numerosTabla = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        int[] tabla;
-        tabla = new int[10];
-        int[] tabla = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        if (numeroIngresado >= 0 & numeroIngresado <= numerosTabla.length) {
 
-        for (int i = 0; numeroIngresado == tabla[i] ; i++) {
-            int calculo = tabla[i] * i;
-            System.out.println(calculo);
+            for (int i = 0; i <= numerosTabla.length ; i++) {
+                int calculo = numeroIngresado * numerosTabla[i];
+                System.out.println(numeroIngresado + "x" + numerosTabla[i] + ":" + calculo);
+            }
+        } else {
+            System.out.println("Solo se adminten numeros entre 0 y 10");
         }
     }
 }
 
 
-// boolean numInorrecto = true;
-//
-// do  {//pido que ingrse valido que ingrese un valor entre 1 y 9
-//     int tabla = teclado.nextInt();
-//     if (tabla > 0 && tabla < 10) {
-//         numInorrecto = false
-//     }
-// } while (numInorrecto)
+/* Formas de definir un arreglo:
+
+// 1ra
+    // Declaro el arreglo
+    int numeroTabla[];
+
+    // Defino el alcance del mismo
+    numeroTabla = new int[2];
+
+    // Defino los valores que va a contener el arreglo por cada indice.
+    numeroTabla[0] = 0;
+    numeroTabla[1] = 1;
+
+// 2da
+    // Declaro el arreglo y defino el alcance del mismo en una sola linea
+    int[] numeroTabla = new int[2];
+
+    // Defino los valores que va a contener el arreglo por cada indice.
+    numeroTabla[0] = 0;
+    numeroTabla[1] = 1;
+
+// 3ra
+    // Defino tanto el arreglo, su alcance y los datos que va a contener
+    int[] numerosTabla = {0, 1};
+*/
