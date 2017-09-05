4d0b1de4ccbbb3427083284be542d32f3d131a90
Guido <cavallo.guido.nicolas@gmail.com>
2017-09-04 21:13:38 -0300

    ejemplo de operadores logicos

---
+++ PasaAlBoliche/PasaAlBoliche.java
@@ -0,0 +1,25 @@
+public static void main(String[] args) {
+
+        boolean b1= true;
+        boolean b2=false;
+        //AND OR NOT
+        System.out.printf("AND ciclo corto && es: %b%n", (b1 && b2));
+        System.out.printf("AND ciclo largo & es: %b%n", (b1 & b2));
+        System.out.printf("OR ciclo corto || es: %b%n", (b1 || b2));
+        System.out.printf("OR ciclo largo | es: %b%n", (b1 | b2));
+        System.out.printf("NOT ! de b1 es: %b%n", !b1);
+        System.out.printf("NOT ! de b2 es: %b%n", !b2);
+
+        boolean mayorDeEdad=true;
+        boolean tieneElDni=false;
+
+        if(mayorDeEdad && tieneElDni){
+            System.out.println("Pasa al boliche");
+        }else{
+            System.out.println("No pasa al boliche");
+        }
+        //Operador ternario
+        String mensaje= (mayorDeEdad && tieneElDni)?"Pasa al boliche":"No pasa";
+        System.out.println(mensaje);
+        System.out.println((mayorDeEdad && tieneElDni)?"Pasa al boliche":"No pasa");
+    }
