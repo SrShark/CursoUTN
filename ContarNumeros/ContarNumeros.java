0bd6c3f0fac19cda00d83652c478300d8652d1ec
Guido Cavallo <SrShark@users.noreply.github.com>
2017-09-11 20:54:46 -0300

    ingresar notas del 1 al 5 y agrupar con asteriscos la cantidad de notas.

---
+++ ContarNumeros/ContarNumeros.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class ContarNumeros {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("ingrese notas del 1 al 5");
+    }
+}
