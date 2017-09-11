ceab0d773a2cf44c7336d4bec3a7f49707fd2f3a
Guido Cavallo <SrShark@users.noreply.github.com>
2017-09-11 19:26:44 -0300

    ejercicio contar letras repetidas

---
+++ ContarLetras/ContarLetras.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ContarLetras {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner (System.in);
+
+        System.out.println("Ingrese la palabra:");
+        String palabra = teclado.next();
+
+        System.out.println("ingrese la letra a buscar:");
+        String letra = teclado.next();
+
+        int contador = 0;
+
+        for (int i = 0; i < palabra.length() ; i++ ) {
+            if (letra.equals(palabra.substring(i, i + 1))){
+                contador += 1;
+            }
+        }
+        System.out.println("se encontraron " + contador +  " coincidencias.");
+    }
+}
