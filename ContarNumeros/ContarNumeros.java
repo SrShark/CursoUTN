da37f952715971ff48133e1b8b1d865c0f039980
Guido Cavallo <SrShark@users.noreply.github.com>
2017-09-11 21:38:32 -0300

    ContarNumeros

--- ContarNumeros/ContarNumeros.java
+++ ContarNumeros/ContarNumeros.java
@@ -4,6 +4,26 @@ public class ContarNumeros {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        System.out.println("ingrese notas del 1 al 5");
+        boolean condicion = true;
+
+        while (condicion == true) {
+            System.out.println("ingrese notas del 1 al 5");
+            int num = teclado.nextInt();
+            switch (num) {
+                case 1: num1 += "* ";
+                break;
+                case 2: num2 += "* ";
+                break;
+                case 3: num3 += "* ";
+                break;
+                case 4: num4 += "* ";
+                break;
+                case 5: num5 += "* ";
+                break;
+                default: condicion = false;
+                break;
+            }
+        }
+        System.out.println(num1);
     }
 }
