8338e10956abcc70cbcc7456531c27d8f1eefee9
Guido Cavallo <cavallo.guido.nicolas@gmail.com>
2017-09-13 10:20:16 -0300

    terminado ejercicio de articulos vendidos.

--- Guia/ArticulosVendidos.java
+++ Guia/ArticulosVendidos.java
@@ -7,25 +7,28 @@ public class ArticulosVendidos {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        String[] articulos = {"articulo 1", "articulo 2", "articulo 3", "articulo 4", "articulo 5"};
-        int[] precio = new int[5];
+        String[] articulos = {"notebook", "televisor", "smartphone", "tablet", "play 4"};
+        double[] precio = new double[5];
         int[] cantidad = new int[5];
+        double[] subtotal = new double[5];
 
-        int total = 0,
-            subtotal = 0;
+        double total = 0;
 
-        for (int i = 0; i < articulos.length ;i++) {
-          System.out.println("ingrese el precio del " + articulos[i]);
-          precio[i] = teclado.nextInt();
-          System.out.println("ingrese la cantidad del " + articulos[i]);
-          cantidad[i] = teclado.nextInt();
+        for (int i = 0; i < articulos.length; i++) {
+            System.out.println("ingrese el precio del " + articulos[i]);
+            precio[i] = teclado.nextDouble();
+            System.out.println("ingrese la cantidad del " + articulos[i]);
+            cantidad[i] = teclado.nextInt();
 
-          subtotal += cantidad[i] * precio[i];
-          total += subtotal;
-
-          String msj = String.format("se vendio %d %s por el total de $%d", cantidad[i], articulos[i], subtotal);
-          System.out.println(msj);
+            subtotal[i] = cantidad[i] * precio[i];
+            total += subtotal[i];
         }
-        System.out.println(total);
+
+        for (int i = 0; i < articulos.length; i++) {
+            String msj = String.format("se vendieron %du. de '%s' por el total de $%.2f", cantidad[i], articulos[i], subtotal[i]);
+            System.out.println(msj);
+        }
+
+        System.out.printf("se vendio un total de $%.2f", total);
     }
 }
