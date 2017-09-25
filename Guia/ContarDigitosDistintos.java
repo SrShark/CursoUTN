85ccd58e73d8d67758ce0defbb8d3e575e21d286
Guido Cavallo <SrShark@users.noreply.github.com>
2017-09-25 19:32:45 -0300

    metodo de busqueada.

--- Guia/ContarDigitosDistintos.java
+++ Guia/ContarDigitosDistintos.java
@@ -1,37 +1,42 @@
-// 14. Escribir un programa que lea un número entero positivo y cuente el número de dígitos distintos que consta.
-
-import java.util.Scanner;
-
 public class ContarDigitosDistintos {
     public static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
 
         // variables
-        int numero = 32133;
+        int numero = 32143;
+        boolean flag = false;
         int suma = 0;
         int[] arrNum = new int[5];
 
         // convierto un entero a string
         String str= Integer.toString(numero);
 
-        // corto el string en digitos y los asigno a un arra para poder manipularlos.
+        // corto el string en digitos y los asigno a un array para poder manipularlos.
         String[] arrStr = str.split("");
 
         for (int i = 0; i < arrStr.length; i++) {
             arrNum[i] = Integer.parseInt(arrStr[i]);
+        }
 
-            for (int n = 0; n < arrNum.length; i++) {
+        for (int i = 0; i < arrNum.length; i++) {
+            for (int n = i + 1; n < arrNum.length; n++) {
+                // creo una instancia del array en memoria para recorrer y comparar si el numero en el indice 0 se repite en algun lado. al repetirlo lo asigno a cero para eliminarlo y seguir recorriendo el array hasta terminar con el 1er indice, y luego compara con el siguiente y asi...
                 if (arrNum[i] == arrNum[n]) {
-                    continue;
-                }
-                else {
-                    int flag = arrNum[n]
+                    // defino la bandera en verdaero, de esta forma indico que se repitio un numero, fuera del for si la bandera esta en verdadero defino el indice que estaba recorriendo en 0 para borrar los numeros que se repitan del array.
+                    arrNum[n] = 0;
+                    flag = true;
                 }
             }
 
-            // suma += arrNum[i];
+            // si la bandera esta en verdadero defino el valor del indice que reccorri anteriormente en 0 para eliminar ese valor del array y asi finalmente sumar solo los numeros que no se repiten.
+            if (flag) {
+                arrNum[i] = 0;
+                flag = false;
+            }
         }
 
+        for (int i = 0; i < arrNum.length; i++) {
+            suma += arrNum[i];
+        }
         System.out.println(suma);
     }
 }
