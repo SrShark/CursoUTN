fb78fd2fdf6b69a03eda7bbe5cdeba580b4c8e4a
Guido Cavallo <SrShark@users.noreply.github.com>
2017-10-03 19:43:26 -0300

    comentado el debug

--- Ordenamiento de vectores/MetodoInsersion.java
+++ Ordenamiento de vectores/MetodoInsersion.java
@@ -4,29 +4,28 @@ public class MetodoInsersion {
     public static void main(String[] args) {
 
         int[] numeros = {30, 15, 2, 21, 44, 1};
-        System.out.println(Arrays.toString(numeros));
-
+        // System.out.println(Arrays.toString(numeros));
 
         for (int i = 1; i < numeros.length; i++) {
-            System.out.println("i: " + i);
+            //System.out.println("i: " + i);
             int aux = numeros[i];
-            System.out.println("aux: " + aux);
+            //System.out.println("aux: " + aux);
             int j = i - 1;
-            System.out.println("j: " + j);
+            //System.out.println("j: " + j);
 
             while (j >= 0 && numeros[j] > aux) {
                 numeros[j + 1] = numeros[j];
-                System.out.printf("while numeros[%d]: %d\n", j, numeros[j + 1]);
-                System.out.println("resultado:" + Arrays.toString(numeros));
+                //System.out.printf("while numeros[%d]: %d\n", j, numeros[j + 1]);
+                //System.out.println("resultado:" + Arrays.toString(numeros));
                 j--;
-                System.out.println("j ahora vale: " + j);
+                //System.out.println("j ahora vale: " + j);
             }
 
-            System.out.println("salio fuera del while");
+            //System.out.println("salio fuera del while");
             numeros[j + 1] = aux;
-            System.out.printf("numeros[%d]: %d\n", (j + 1), aux);
-            System.out.println("resultado:" + Arrays.toString(numeros));
-            System.out.println("\n");
+            //System.out.printf("numeros[%d]: %d\n", (j + 1), aux);
+            System.out.println("resultado: " + Arrays.toString(numeros));
+            //System.out.println("\n");
         }
 
         System.out.println(Arrays.toString(numeros));
