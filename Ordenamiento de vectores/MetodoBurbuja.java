c104224faf3c4343a4670ba0ed07c025fdaf1cef
Guido Cavallo <SrShark@users.noreply.github.com>
2017-10-03 19:50:21 -0300

    cambio menor

---
+++ Ordenamiento de vectores/MetodoBurbuja.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+public class MetodoInsersion {
+    public static void main(String[] args) {
+
+        int[] numeros = {30, 15, 2, 21, 44, 1};
+
+        System.out.println("final: " + Arrays.toString(numeros));
+    }
+}
