6595deba4894c15a6b409e6d6c1cc2b71fbd69ad
Guido Cavallo <SrShark@users.noreply.github.com>
2017-10-03 19:41:30 -0300

    ejercicio de ordenamiento: metodo de inserisión con debug.

--- Ordenamiento de vectores/MetodoInsersion.java
+++ Ordenamiento de vectores/MetodoInsersion.java
@@ -1,37 +1,34 @@
+import java.util.Arrays;
+
 public class MetodoInsersion {
     public static void main(String[] args) {
-        int[] numeros = {8, 3, 9, 2, 6};
-        int menor = 0, original = 0;
-        boolean flag = false;
 
-        for (int i = 0; i < numeros.length; i++) {
-            //System.out.println("i: " + numeros[i]);
+        int[] numeros = {30, 15, 2, 21, 44, 1};
+        System.out.println(Arrays.toString(numeros));
 
-            for (int n = i + 1; n < numeros.length; n++) {
-                // System.out.println("n: " + numeros[n]);
 
-                original = numeros[i];
+        for (int i = 1; i < numeros.length; i++) {
+            System.out.println("i: " + i);
+            int aux = numeros[i];
+            System.out.println("aux: " + aux);
+            int j = i - 1;
+            System.out.println("j: " + j);
 
-                if (numeros[i] > numeros[n]) {
-                    menor = numeros[n];s
-                    flag = true;
-                    break;
-                }
+            while (j >= 0 && numeros[j] > aux) {
+                numeros[j + 1] = numeros[j];
+                System.out.printf("while numeros[%d]: %d\n", j, numeros[j + 1]);
+                System.out.println("resultado:" + Arrays.toString(numeros));
+                j--;
+                System.out.println("j ahora vale: " + j);
             }
 
-            if (flag) {
-                numeros[i] = menor;
-                numeros[i + 1] = original;
-                flag = false;
-            }
-
-        System.out.println(numeros[i]);
+            System.out.println("salio fuera del while");
+            numeros[j + 1] = aux;
+            System.out.printf("numeros[%d]: %d\n", (j + 1), aux);
+            System.out.println("resultado:" + Arrays.toString(numeros));
+            System.out.println("\n");
         }
 
+        System.out.println(Arrays.toString(numeros));
     }
 }
-
-// 8 3 9 2 6
-// 3 8 9
-// 2 3 8 9
-// 2 3 6 8 9
