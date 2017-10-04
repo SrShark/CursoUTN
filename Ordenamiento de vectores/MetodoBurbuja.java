69d2107ebcf66812e6f0e2df429fe38e971e1680
Guido Cavallo <SrShark@users.noreply.github.com>
2017-10-03 21:33:45 -0300

    metodo burbuja

--- Ordenamiento de vectores/MetodoBurbuja.java
+++ Ordenamiento de vectores/MetodoBurbuja.java
@@ -1,10 +1,24 @@
 import java.util.Arrays;
 
-public class MetodoInsersion {
+public class MetodoBurbuja {
     public static void main(String[] args) {
 
-        int[] numeros = {30, 15, 2, 21, 44, 1};
+        int[] numeros = {5, 4, 3, 2, 1};
+        int aux;
 
-        System.out.println("final: " + Arrays.toString(numeros));
+        for (int i = 0; i < numeros.length - 1; i++) {
+
+            for (int n = 0; n < numeros.length - 1; n++) {
+
+                if (numeros[n] > numeros[n + 1]) {
+                    aux = numeros[n];
+                    numeros[n] = numeros[n + 1];
+                    numeros[n + 1] = aux;
+                }
+                System.out.println("final: " + Arrays.toString(numeros));
+            }
+        }
+
+        // System.out.println("final: " + Arrays.toString(numeros));
     }
 }
